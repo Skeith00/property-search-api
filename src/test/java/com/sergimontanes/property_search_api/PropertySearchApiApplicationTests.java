500010665bc158360f797dcad1daedd757d2ee6e
Sergi <smdt505@hotmail.com>
2024-12-17 22:05:13 +1000

    git commit -m "Initial commit"

---
+++ src/test/java/com/sergimontanes/property_search_api/PropertySearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergimontanes.property_search_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
