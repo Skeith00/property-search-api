47dbec74c08a4b84e714fac9f37567bc3f105e31
Sergi <smdt505@hotmail.com>
2025-03-10 16:49:11 +1000

    Change return types for Controllers

--- src/main/java/com/sergimontanes/api/controller/PropertyController.java
+++ src/main/java/com/sergimontanes/api/controller/PropertyController.java
@@ -5,6 +5,7 @@ import com.sergimontanes.api.dto.PropertySearchDTO;
 import com.sergimontanes.api.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,22 +22,18 @@ public class PropertyController {
     }
 
     @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String storeProperty(@RequestBody PropertyDTO property) {
-        try {
-            propertyService.storeProperty(property);
-            return "Search executed, check logs for results.";
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<String> storeProperty(@RequestBody PropertyDTO property) {
+        propertyService.storeProperty(property);
+        return ResponseEntity.ok("OK");
     }
-    @PostMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<PropertyDTO> search(@RequestBody PropertySearchDTO search) {
-        return propertyService.searchProperties(search);
+
+    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PropertyDTO>> search(@RequestBody PropertySearchDTO search) {
+        return ResponseEntity.ok(propertyService.searchProperties(search));
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public PropertyDTO getProperty(@RequestParam Long propertyId) {
-        return propertyService.getProperty(propertyId);
+    public ResponseEntity<PropertyDTO> getProperty(@PathVariable Long id) {
+        return ResponseEntity.ok(propertyService.getProperty(id));
     }
-
 }
